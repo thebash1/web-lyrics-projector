6c90a2055fade3772d7b3541cfb74fff6da8d599
Alex Daniel Mora <alex.daniel50904@gmail.com>
2026-06-16 01:23:09 -0500

    fix: configure dinamic port for render

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,13 @@ import node from '@astrojs/node';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-
+  adapter: node({
+    mode: 'standlone',
+  }),
+  server: {
+    port: process.env.PORT ? parseInt(process.env.PORT) : 4321,
+    host: true,
+  }
   integrations: [
     // Tailwind CSS integration
     (await import('@astrojs/tailwind')).default(),
