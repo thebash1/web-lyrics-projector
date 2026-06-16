c26aa6cc1b982cc2c08331b8e63bad7be4ffbb61
Alex Daniel Mora <alex.daniel50904@gmail.com>
2026-06-16 01:12:04 -0500

    feat: add node adapter for render

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+
   integrations: [
     // Tailwind CSS integration
     (await import('@astrojs/tailwind')).default(),
   ],
+
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
